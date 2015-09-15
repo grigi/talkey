d156052f5a5d465b18e94e3eab8c270a2927c14e
Grigi <nagrigoriadis@gmail.com>
2015-09-15 09:16:14 +0200

    Install deps in travis

--- talkey/test_suite.py
+++ talkey/test_suite.py
@@ -26,15 +26,36 @@ class DummyTTSTest(unittest.TestCase):
     def test_instantiate_blank(self):
         cls = self.CLS
         self.assertEqual(cls.SLUG, self.SLUG)
-        self.assertEqual(list(cls.get_init_options().keys()), self.INIT_ATTRS)
+        self.assertEqual(sorted(cls.get_init_options().keys()), sorted(self.INIT_ATTRS))
         obj = cls(**self.CONF)
         self.assertEqual(obj.SLUG, self.SLUG)
         self.assertEqual(obj.is_available(), True)
-        self.assertEqual(list(obj.get_options().keys()), self.OBJ_ATTRS)
+        self.assertEqual(sorted(obj.get_options().keys()), sorted(self.OBJ_ATTRS))
         language, voice, voiceinfo, options = obj.configure()
         self.assertEqual(language, 'en')
         self.assertIsNotNone(voice)
         self.assertEqual(voice, obj.get_languages()['en']['default'])
-        self.assertEqual(list(options.keys()), self.OBJ_ATTRS)
+        self.assertEqual(sorted(options.keys()), sorted(self.OBJ_ATTRS))
 
-        
+
+#class FestivalTTSTest(DummyTTSTest):
+    #CLS = FestivalTTS
+    #SLUG = 'festival-tts'
+    #INIT_ATTRS = []
+    #CONF = {}
+    #OBJ_ATTRS = []
+
+#class FliteTTSTest(DummyTTSTest):
+    #CLS = FliteTTS
+    #SLUG = 'flite-tts'
+    #INIT_ATTRS = []
+    #CONF = {}
+    #OBJ_ATTRS = []
+
+#class EspeakTTSTest(DummyTTSTest):
+    #CLS = EspeakTTS
+    #SLUG = 'espeak-tts'
+    #INIT_ATTRS = []
+    #CONF = {}
+    #OBJ_ATTRS = ['words_per_minute', 'pitch_adjustment', 'variant']
+
