9fb2772bd90fa94716002c4f100799bea9d15b59
Grigi <nagrigoriadis@gmail.com>
2015-10-06 10:10:19 +0200

    py35 support

--- talkey/test_suite.py
+++ talkey/test_suite.py
@@ -340,7 +340,7 @@ class MaryTTSTest(BaseTTSTest):
     CLS = MaryTTS
     SLUG = 'mary'
     INIT_ATTRS = ['enabled', 'host', 'port', 'scheme']
-    CONF = {'enabled': True}#, 'host': 'mary.dfki.de'}
+    CONF = {'enabled': True, 'host': 'mary.dfki.de'}
     EVAL_PLAY = True
     SKIP_IF_NOT_AVAILABLE = True
 
